c2e41f2d234630a91f2d53fe86de04654e0ff05d
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-06-23 14:20:42 -0500

    changing activity

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     console.log('Ready!');
-    client.user.setActivity("Music", {type: "LISTENING"});
+    client.user.setActivity("/help", {type: "LISTENING"});
 });
 
 client.on('interactionCreate', async interaction => {
