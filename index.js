29b68d80d3a79cfe28672b52682230608dbf4f71
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-06-28 03:02:32 -0500

    champing activity

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     console.log('Ready!');
-    client.user.setActivity("/help", {type: "LISTENING"});
+    client.user.setActivity("/help", {type: "COMPETING"});
 });
 
 client.on('interactionCreate', async interaction => {
